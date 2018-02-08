8a39e5eaf8207fa88974446789bdeda23a9fb12b
Aooooooy <512224856@qq.com>
2018-02-08 17:57:47 +0800

    添加饿了么UI的css

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-        navbar
+    <div id="navBar">
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+              <el-menu-item index="1"><a href="#">图片</a></el-menu-item>
+              <el-menu-item index="2"><a href="#">交易中心</a></el-menu-item>
+              <el-menu-item index="3"><a href="#">帮助中心</a></el-menu-item>
+              <el-menu-item index="4"><a href="#">公告中心</a></el-menu-item>
+              <el-submenu index="5">
+                <template slot="title">最新活动</template>
+                <el-menu-item index="5-1"><a href="#" target="_blank">矿机售卖</a></el-menu-item>
+                <el-menu-item index="5-2"><a href="#" target="_blank">HOTC排名赛</a></el-menu-item>
+                <el-menu-item index="5-3"><a href="#" target="_blank">DCON排名赛</a></el-menu-item>
+                <el-menu-item index="5-4"><a href="#" target="_blank">SDD排名赛</a></el-menu-item>
+                <el-menu-item index="5-5"><a href="#" target="_blank">SHE排名赛</a></el-menu-item>
+                <el-menu-item index="5-6"><a href="#" target="_blank">AIT排名赛</a></el-menu-item>
+              </el-submenu>
+              <el-submenu index="6">
+                <template slot="title">法币交易</template>
+                <el-menu-item index="6-1"><a href="#" target="_blank">CK.USD & ETH & BTC</a></el-menu-item>
+                <el-menu-item index="6-2"><a href="#" target="_blank">代理充值</a></el-menu-item>
+              </el-submenu>
+              <el-menu-item index="7"><a href="#" target="_blank">(登陆 | 注册)</a></el-menu-item>
+              <el-submenu index="8">
+                <template slot="title">当前语言</template>
+                <el-menu-item index="8-1">香港</el-menu-item>
+                <el-menu-item index="8-2">英国</el-menu-item>
+              </el-submenu>
+        </el-menu>
     </div>
 </template>
 
@@ -17,11 +42,15 @@ export default {
     },
     data () {
         return {
+            activeIndex: '1'
         }
     },
     components: {
     },
     methods: {
+        handleSelect(key, keyPath) {
+            console.log(key,keyPath);
+        }
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
